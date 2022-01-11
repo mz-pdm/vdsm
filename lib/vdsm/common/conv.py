73003374fc3a6b7b35b8cdf0a520aef4b0f5ea68
Milan Zamazal <mzamazal@redhat.com>
2022-01-11 09:18:54 +0100

    put something back

--- lib/vdsm/common/conv.py
+++ lib/vdsm/common/conv.py
@@ -18,4 +18,13 @@
 #
 
 def tobool(s):
-    return bool(s)
+    try:
+        if s is None:
+            return False
+        if type(s) == bool:
+            return s
+        if s.lower() == 'true':
+            return True
+        return bool(int(s))
+    except:
+        return False
